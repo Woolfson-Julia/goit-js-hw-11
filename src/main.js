3df7a8bc657d324f5f9bf342b7075a1176edfa0e
Woolf <woolfson1995@gmail.com>
2025-01-21 22:43:24 +0200

    hw11

--- src/main.js
+++ src/main.js
@@ -5,20 +5,40 @@ import SimpleLightbox from "simplelightbox";
 import { createGalleryCardTemplate } from './js/render-functions';
 import { fetchPhotosByQuery } from './js/pixabay-api'
 
+
+
+
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
+const loaderEl = document.querySelector('.js-loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`, 
+  captionDelay: 250,
+});
+const toggleLoader = (isLoading) => {
+  if (isLoading) {
+    loaderEl.classList.remove('visually-hidden'); // Показываем загрузчик
+  } else {
+    loaderEl.classList.add('visually-hidden'); // Скрываем загрузчик
+  }
+};
 
 const onSearchFormSubmit = event => {
+  // loader.classList.toggle(`visually-hidden`);
   event.preventDefault();
   const searchedQuery = event.currentTarget.user_query.value.trim();
-
+  toggleLoader(true);
   fetchPhotosByQuery(searchedQuery)
     .then(data => {
+      toggleLoader(false);
       if (data.total === 0) {
-        iziToast.info({
+        
+
+        iziToast.error({
+        position: 'topRight', 
         message: "Sorry, there are no images matching your search query. Please try again!" 
         })
-
 
         galleryEl.innerHTML = '';
 
@@ -29,26 +49,29 @@ const onSearchFormSubmit = event => {
 
       const galleryTemplate = data.hits.map(el => createGalleryCardTemplate(el)).join('');
       galleryEl.innerHTML = galleryTemplate;
+      // Оновлюємо SimpleLightbox після додавання нових карток
+      lightbox.refresh();
     })
     .catch(err => {
       console.log(err);
+      toggleLoader(false);
     });
 };
 
-// const onModal = event => {
-//   event.preventDefault();
-//   if (event.target.nodeName === 'IMG') {
-//     const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <img class="product-modal-img" src="${event.target.dataset.source}" alt="${event.target.alt}" />
-//     </div>
-// `);
+const input = document.querySelector('.js-search-input');
 
-// instance.show()
-//   }
-// };
+
+input.addEventListener('input', () => {
+  if (input.value !== "") {
+    input.classList.add('input-typing');  // Синяя рамка при вводе текста
+  } else {
+    input.classList.remove('input-typing');  // Убираем синий цвет, если поле пустое
+  }
+});
+
+
+
 
 
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
 
-// galleryEl.addEventListener('click', onModal);
